47d9a3c5328bc3f037f4e85e3c48d06f00f9700f
Anthony Dillon <me@anthonydillon.com>
2015-06-07 23:27:08 +0100

    Add navigation functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,55 @@
 $(document).ready(function() {
-	console.log('test');
-	console.log($(document).scrollTop());
+	var siteHeader = $('.site-header'),
+		buyRow = $('#buy').position().top,
+		productInfoRow = $('#product-info').position().top,
+		shippingRow = $('#shipping').position().top,
+		galleryRow = $('#gallery').position().top,
+		faqRow = $('#faq').position().top,
+		contactRow = $('#contact').position().top,
+		currentRow = false;
+
+	console.log(contactRow);
+
+	function checkNav() {
+		var scroll = $(document).scrollTop() + 50,
+			cRow = false;
+
+		if (scroll > 100) {
+			siteHeader.addClass('scrolled');
+		} else {
+			siteHeader.removeClass('scrolled');
+		}
+
+		if (scroll > contactRow) {
+			cRow = 'contact';
+		} else if (scroll > faqRow) {
+			cRow = 'faq';
+		} else if (scroll > galleryRow) {
+			cRow = 'gallery';
+		} else if (scroll > shippingRow) {
+			cRow = 'shipping';
+		} else if (scroll > productInfoRow) {
+			cRow = 'product-info';
+		} else if (scroll > buyRow) {
+			cRow = 'buy';
+		} else {
+			cRow = false;
+		}
+
+		if(currentRow != cRow) {
+			currentRow = cRow;
+			$('.site-nav a').removeClass('active');
+			if (cRow) {
+				$('.site-nav a[href="#'+currentRow+'"]').addClass('active');
+			}
+		}
+	}
+
+	$(document).scroll(function() {
+		checkNav();
+
+	});
+
+	checkNav();
 
 });
-
-console.log('loaded')
